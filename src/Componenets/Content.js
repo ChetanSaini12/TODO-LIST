39484c9ed70c99fcd950f721e2253dbfb3b69757
chetansaini <chetansaini1241@gmail.com>
2023-02-04 10:38:19 +0530

    donegreen

--- src/Componenets/Content.js
+++ src/Componenets/Content.js
@@ -1,171 +1,182 @@
 import React, { useState } from "react";
 import Task from "./Task";
-import './task.css'
+import "./task.css";
 
 let allTask = [
-  {
-    id: 1,
-    task: "Give dog a bath",
-    complete: false,
-  },
-  {
-    id: 2,
-    task: "Do laundry",
-    complete: false,
-  },
-  {
-    id: 3,
-    task: "Vacuum floor",
-    complete: false,
-  },
-  {
-    id: 4,
-    task: "Feed cat",
-    complete: false,
-  },
-  {
-    id: 5,
-    task: "Change light bulbs",
-    complete: false,
-  },
-  {
-    id: 6,
-    task: "Go to Store",
-    complete: false,
-  },
-  {
-    id: 7,
-    task: "Fill gas tank",
-    complete: false,
-  },
-  {
-    id: 8,
-    task: "Change linens",
-    complete: false,
-  },
-  {
-    id: 9,
-    task: "Rake leaves",
-    complete: false,
-  },
-  {
-    id: 10,
-    task: "Bake Cookies",
-    complete: false,
-  },
-  {
-    id: 11,
-    task: "Take nap",
-    complete: false,
-  },
-  {
-    id: 12,
-    task: "Read book",
-    complete: false,
-  },
-  {
-    id: 13,
-    task: "Exercise",
-    complete: false,
-  },
-  {
-    id: 14,
-    task: "Give dog a bath",
-    complete: false,
-  },
-  {
-    id: 15,
-    task: "Do laundry",
-    complete: false,
-  },
-  {
-    id: 16,
-    task: "Vacuum floor",
-    complete: false,
-  },
-  {
-    id: 17,
-    task: "Feed cat",
-    complete: false,
-  },
-  {
-    id: 18,
-    task: "Change light bulbs",
-    complete: false,
-  },
-  {
-    id: 19,
-    task: "Go to Store",
-    complete: false,
-  },
-  {
-    id: 20,
-    task: "Fill gas tank",
-    complete: false,
-  },
+  // {
+  //   id: 1,
+  //   task: "Give dog a bath",
+  //   complete: false,
+  // },
+  // {
+  //   id: 2,
+  //   task: "Do laundry",
+  //   complete: false,
+  // },
+  // {
+  //   id: 3,
+  //   task: "Vacuum floor",
+  //   complete: false,
+  // },
+  // {
+  //   id: 4,
+  //   task: "Feed cat",
+  //   complete: false,
+  // },
+  // {
+  //   id: 5,
+  //   task: "Change light bulbs",
+  //   complete: false,
+  // },
+  // {
+  //   id: 6,
+  //   task: "Go to Store",
+  //   complete: false,
+  // },
+  // {
+  //   id: 7,
+  //   task: "Fill gas tank",
+  //   complete: false,
+  // },
+  // {
+  //   id: 8,
+  //   task: "Change linens",
+  //   complete: false,
+  // },
+  // {
+  //   id: 9,
+  //   task: "Rake leaves",
+  //   complete: false,
+  // },
+  // {
+  //   id: 10,
+  //   task: "Bake Cookies",
+  //   complete: false,
+  // },
+  // {
+  //   id: 11,
+  //   task: "Take nap",
+  //   complete: false,
+  // },
+  // {
+  //   id: 12,
+  //   task: "Read book",
+  //   complete: false,
+  // },
+  // {
+  //   id: 13,
+  //   task: "Exercise",
+  //   complete: false,
+  // },
+  // {
+  //   id: 14,
+  //   task: "Give dog a bath",
+  //   complete: false,
+  // },
+  // {
+  //   id: 15,
+  //   task: "Do laundry",
+  //   complete: false,
+  // },
+  // {
+  //   id: 16,
+  //   task: "Vacuum floor",
+  //   complete: false,
+  // },
+  // {
+  //   id: 17,
+  //   task: "Feed cat",
+  //   complete: false,
+  // },
+  // {
+  //   id: 18,
+  //   task: "Change light bulbs",
+  //   complete: false,
+  // },
+  // {
+  //   id: 19,
+  //   task: "Go to Store",
+  //   complete: false,
+  // },
+  // {
+  //   id: 20,
+  //   task: "Fill gas tank",
+  //   complete: false,
+  // },
 ];
 
-let x = 21;
+let x = 1;
 function Content() {
-
-
   let handleSubmit = (event) => {
     event.preventDefault();
-    
+
     setItems([...items, { text: inputValue }]);
     setInputValue("");
     console.log({ inputValue });
     console.log("hello");
-    if(inputValue == "")
-    {
+    if (inputValue == "") {
       return;
       console.log(x);
     }
     let tmp = {
       id: x,
       task: inputValue,
-      complete: false,
-      
+      complete: true,
+      urgent: true,
     };
-    x= x+1;
+    x = x + 1;
     allTask.push(tmp);
+
     console.log(JSON.stringify(allTask, null, 4));
   };
-  
+
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
   };
 
-  const [clear,setClear]=useState(false);
+  const [clear, setClear] = useState(false);
   const clearArray = (event) => {
     event.preventDefault();
-    while(allTask.length > 0) {
+    while (allTask.length > 0) {
       allTask.pop();
-  }
+    }
+    x=1;
     setClear(!clear);
     console.log("clear huva h");
-  }
+  };
 
   // const [value, setValue] = React.useState("");
   const [inputValue, setInputValue] = React.useState("");
   const [items, setItems] = React.useState([]);
 
   return (
-    <div className=" bg-inherit fixed w-11/12 h-full my-20 mt-20 grid v-screen place-items-center" style={{marginTop:"3rem"}}>
-      <div className="w-9/12  rounded-2xl h-screen my-10 flex justify-center   mt-10  bg-gradient-to-br from-[#386D62] via-[#21444F] to-[#08172B] " style={{height:"28rem"}}>
+    <div
+      className=" bg-inherit fixed w-11/12 h-full my-20 mt-20 grid v-screen place-items-center"
+      style={{ marginTop: "3rem" }}
+    >
+      <div
+        className="w-9/12  rounded-2xl h-screen my-10 flex justify-center   mt-10  bg-gradient-to-br from-[#386D62] via-[#21444F] to-[#08172B] "
+        style={{ height: "28rem" }}
+      >
         <div className="form-control w-full scrollbar-hide scrollbarHide ">
-          <label className="input-group place-content-center mt-5 mb-5 ">
+          <label id="txtSearch" className="input-group place-content-center mt-5 mb-5 ">
             <input
+              autofocus="autofocus"
               type="text"
               placeholder="Write here..."
               className="input w-9/12  input-group-lg input-bordered"
               value={inputValue}
               onChange={handleInputChange}
             />
-            <span className="btn" onClick={handleSubmit}>
+            <span id="btnSearch" className="btn" onClick={handleSubmit}>
               Add
             </span>
-            <button className="btn ml-2 btn-outline btn-info" onClick={clearArray} >Clear All</button>
+            <button
+              className="btn ml-2 btn-outline btn-info"
+              onClick={clearArray}
+            >
+              Clear All
+            </button>
           </label>
           <Task ary={allTask} />
         </div>
--- src/Componenets/Task.js
+++ src/Componenets/Task.js
@@ -1,23 +1,23 @@
 import React from "react";
-import './task.css'
+import "./task.css";
 
 let txt = "Done";
 
-let grn 
+let grn;
 
 function Task(props) {
-  const [isDone, setisDone] = React.useState(false); 
-  
+  const [isDone, setisDone] = React.useState(false);
+
   // let chngDone = (event) => {
-    //   txt = "Not Done";
-    //   alert("Huva");
-    // }
-    function chngDone(){
-      // document.getElementById("dn").innerHTML = "Not Done";
-      txt = "Not Done";
-    }
-    return (
-      <div className="overflow-scroll overflow-x-auto hideScrollbar">
+  //   txt = "Not Done";
+  //   alert("Huva");
+  // }
+  function chngDone() {
+    // document.getElementById("dn").innerHTML = "Not Done";
+    txt = "Not Done";
+  }
+  return (
+    <div className="overflow-scroll overflow-x-auto hideScrollbar">
       <table className="table table-zebra rounded-none top-0 px-6 py-4 w-full">
         {/* <thead>
           <tr>
@@ -29,65 +29,94 @@ function Task(props) {
           <th className="sticky top-0 px-6 py-3 text-blue-900 bg-blue-300"></th>   
           </tr>
         </thead> */}
-        <tbody > 
+        <tbody>
           {props.ary.map((tsk) => {
-            // setisDone(false)
-            if(tsk.complete)
-            return (
-              <tr key={tsk.id}  bgcolor="green" style={{backgroundColor:"green"}}>
-                    <td></td>
-                    <th>{tsk.id}.</th>
-                    <td>{tsk.task}</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td>
-                    <button id="dn" className="btn btn-xs btn-outline btn-success" onClick={() =>{
-                        tsk.complete=!tsk.complete;
+            //..
+            //..
+            //..
+            //..
+            //..
+            if (tsk.complete)
+              return (
+                <tr key={tsk.id}>
+                  <td></td>
+                  <th>{tsk.id}.</th>
+                  <td>{tsk.task}</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td>
+                    <button
+                      id="dn"
+                      className="btn btn-xs btn-outline btn-success"
+                      onClick={() => {
+                        tsk.complete = !tsk.complete;
                         setisDone(!isDone);
-
-                    } } >{tsk.complete ? "!Done" : "Done" }</button>
-                    </td>
-                    <td>
-                    <button className="btn btn-xs btn-outline btn-warning">Urgent</button>
-                    </td>
-                    <td>
-                    <button className="btn btn-xs btn-outline btn-error">Delete</button>
-                    </td>
+                      }}
+                    >
+                      Done
+                    </button>
+                  </td>
+                  <td>
+                    <button className="btn btn-xs btn-outline btn-warning">
+                      Urgent
+                    </button>
+                  </td>
+                  <td>
+                    <button className="btn btn-xs btn-outline btn-error">
+                      Delete
+                    </button>
+                  </td>
                 </tr>
-            );
+              );
+            //..
             else
-            return (
-              <tr key={tsk.id} >
-                    <td></td>
-                    <th>{tsk.id}.</th>
-                    <td>{tsk.task}</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td>
-                    <button id="dn" className="btn btn-xs btn-outline btn-success" onClick={() =>{
-                        tsk.complete=!tsk.complete;
+              return (
+                <tr key={tsk.id}>
+                  <td style={{ backgroundColor: "hsl(var(--su))", color: "black" }}></td>
+                  <th style={{ backgroundColor: "hsl(var(--su))", color: "black" }}>{tsk.id}.</th>
+                  <td style={{ backgroundColor: "hsl(var(--su))", color: "black" }}>{tsk.task}</td>
+                  <td style={{ backgroundColor: "hsl(var(--su))", color: "black" }}></td>
+                  <td style={{ backgroundColor: "hsl(var(--su))", color: "black" }}></td>
+                  <td style={{ backgroundColor: "hsl(var(--su))", color: "black" }}></td>
+                  <td style={{ backgroundColor: "hsl(var(--su))", color: "black" }}></td>
+                  <td style={{ backgroundColor: "hsl(var(--su))", color: "black" }}></td>
+                  <td style={{ backgroundColor: "hsl(var(--su))", color: "black" }}></td>
+                  <td style={{ backgroundColor: "hsl(var(--su))", color: "black" }}></td>
+                  <td style={{ backgroundColor: "hsl(var(--su))"}}>
+                    <button style={{ color: "black"}} 
+                      id="dn"
+                      className="btn btn-xs btn-outline btn-success"
+                      onClick={() => {
+                        tsk.complete = !tsk.complete;
                         setisDone(!isDone);
-
-                    } } >{tsk.complete ? "!Done" : "Done" }</button>
-                    </td>
-                    <td>
-                    <button className="btn btn-xs btn-outline btn-warning">Urgent</button>
-                    </td>
-                    <td>
-                    <button className="btn btn-xs btn-outline btn-error">Delete</button>
-                    </td>
+                      }}
+                    >
+                      !DONE
+                    </button>
+                  </td>
+                  <td style={{ backgroundColor: "hsl(var(--su))"}}>
+                    <button  style={{ color: "black",  opacity: 0.6 }} disabled className="btn pointer-events-none cursor-not-allowed btn-xs btn-outline btn-warning">
+                      Urgent
+                    </button>
+                  </td>
+                  <td style={{ backgroundColor: "hsl(var(--su))"}}>
+                    <button  style={{ color: "black"}} className="btn btn-xs btn-outline btn-error">
+                      Delete
+                    </button>
+                  </td>
                 </tr>
-            );
+              );
+              //..
+              //..
+              //..
+              //..
+              //..
+              
           })}
         </tbody>
       </table>
